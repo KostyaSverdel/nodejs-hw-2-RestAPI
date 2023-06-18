a22cbde3721d29f48ffca42a6b9c52d56c6992ed
KostyaSverdel <109824269+KostyaSverdel@users.noreply.github.com>
2023-06-18 14:15:34 +0300

    start-hw

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
 const app = require('./app')
-
+const DB_Host = "mongodb+srv://Kostya:GwuEWSFufrgeh7VI@cluster0.mrsfz97.mongodb.net/db-contacts?retryWrites=true&w=majority";
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
+})
+mongoose.connect(DB_Host)
+.then (()=>
+{app.listen(3000);
+})
+.catch(error =>{
+  console.log (error.message);
+  process.exit(1);
 })
